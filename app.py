ae5b67debf15ef8fef606718afb2edd881668a1e
Joelkohym <56166111+Joelkohym@users.noreply.github.com>
2023-09-01 06:59:36 +0000

    add loginpage

--- app.py
+++ app.py
@@ -16,10 +16,8 @@ app = Flask(__name__)
 
 @app.route("/login")
 def hello_user():
-  username = ""
-
-  
-  return ""
+  user_data = load_data_from_db()
+  return render_template('login.html', user_data = user_data)
   
 colors = [
 "red","blue","green","purple","orange","darkred","lightred","beige","darkblue","darkgreen","cadetblue","darkpurple","white","pink","lightblue","lightgreen","gray","black","lightgray"
