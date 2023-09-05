cd0d8706583de67f831068588650be7d68e3a784
Joelkohym <56166111+Joelkohym@users.noreply.github.com>
2023-09-05 08:10:05 +0000

    changed login() redirect to render_template

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def login():
     # if request.data['username'] and request.data['password'] in db:
     #   user_data = load_data_from_db()
   if request.method == 'GET':
-    return redirect(url_for('login'))
+    return render_template('login.html')
   
 colors = [
 "red","blue","green","purple","orange","darkred","lightred","beige","darkblue","darkgreen","cadetblue","darkpurple","white","pink","lightblue","lightgreen","gray","black","lightgray"
