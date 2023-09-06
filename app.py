119b590f9bdc10c4529b72edfe0bcb0983c0d672
Joelkohym <56166111+Joelkohym@users.noreply.github.com>
2023-09-06 09:55:50 +0000

    changed delay to 3 seconds

--- app.py
+++ app.py
@@ -417,7 +417,7 @@ def Vessel_map():
       # Read data from 'Sheet2' into another DataFrame
       df2 = pd.DataFrame(sheet2.get_all_records())
       print(f"df2 = {df2}")
-      time.sleep(1)
+      time.sleep(3)
     
     # Assuming 'imo_no' is the common column
     merged_df = pd.merge(df1,
