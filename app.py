d6cfc5074c4fe69f3af6cf1b5c61b24d20c86b2d
Joelkohym <56166111+Joelkohym@users.noreply.github.com>
2023-09-06 10:13:26 +0000

    print sleep

--- app.py
+++ app.py
@@ -410,6 +410,7 @@ def Vessel_map():
     # Read data from 'Sheet2' into another DataFrame
     df2 = pd.DataFrame(sheet2.get_all_records())
     print(f"df2 = {df2}")
+    print("Sleep 15s")
     time.sleep(15)
     # Read data from 'Sheet1' into a DataFrame
     while df1.empty or df2.empty:
