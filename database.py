02f6aee96cebab5d30e7e2bb042f172917a17461
Joelkohym <56166111+Joelkohym@users.noreply.github.com>
2023-09-07 09:24:56 +0000

    testDB

--- database.py
+++ database.py
@@ -91,3 +91,110 @@ def receive_details(email):
     receive_data = receive_db.all()[0]
     print(f"receive_data == {receive_data}, return api ={receive_data[3]}, return pID = {receive_data[4]}, return pitstop = {receive_data[5]}")
   return(receive_data[0],receive_data[3],receive_data[4],receive_data[5],receive_data[6])
+
+
+def new_vessel_current_position(data, email):
+  with engine.connect() as conn:
+    query = text("select * from userDB WHERE email = :email")
+    values = {'email' : email}
+    result = conn.execute(query, values)
+    result_all = result.all()
+    print(result_all)
+    print(f"length of result all = {len(result_all)}")
+    if len(result_all) > 0:
+      db_connection_string_vcp = result_all[0][6]
+      engine_vcp = create_engine(db_connection_string_vcp,connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}})
+      with engine.connect() as conn:
+        query = text("INSERT INTO vessel_current_position_UCE (    vessel_nm,vessel_imo_no,vessel_flag,vessel_call_sign,vessel_location_from,vessel_location_to,vessel_movement_height,vessel_movement_type,vessel_movement_start_dt,vessel_movement_end_dt,vessel_movement_status,vessel_movement_draft,Timestamp_vessel_movement) VALUES (:vessel_nm,:vessel_imo_no,:vessel_flag,:vessel_call_sign,:vessel_location_from,:vessel_location_to,:vessel_movement_height,:vessel_movement_type,:vessel_movement_start_dt,:vessel_movement_end_dt,:vessel_movement_status,:vessel_movement_draft,:Timestamp_vessel_movement)")
+        values = {'vessel_nm,vessel_imo_no':data['vessel_nm,vessel_imo_no'],vessel_flag,vessel_call_sign,vessel_location_from,vessel_location_to,vessel_movement_height,vessel_movement_type,vessel_movement_start_dt,vessel_movement_end_dt,vessel_movement_status,vessel_movement_draft,Timestamp_vessel_movement
+        values = {'email' : data['email'], 'password' : data['password'], 'api_key' : data['api_key'], 'participant_id' :data['participant_id'], 'pitstop_url':data['pitstop_url'],'gsheet_cred_path' : data['gsheet_cred_path']}
+        print(query)
+        result = conn.execute(query, values)
+      print("execute success")
+      return 1
+    else:
+      print('User exists, please try again')
+      return 0
+
+
+    email = email_url
+    receive_details_data = receive_details(email)
+    print(f"Receive_details from database.py {receive_details(email)}")
+    API_KEY = receive_details_data[1]
+    participant_id = receive_details_data[2]
+    pitstop_url = receive_details_data[3]
+    gsheet_cred_path = receive_details_data[4]
+  
+    data = request.data  # Get the raw data from the request body
+    
+    print(f"Vessel_current_position = {data}")
+
+    data_str = data.decode('utf-8')  # Decode data as a UTF-8 string
+    # Convert the JSON string to a Python dictionary
+    data_dict = json.loads(data_str)
+    row_data_vessel_current_position = data_dict['payload'][-1]
+    # Add the current date and time to your data dictionary
+    current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    row_data_vessel_current_position['Timestamp vessel_current_position'] = str(current_datetime)
+    
+    print(
+      f"row_data_vessel_current_position: {row_data_vessel_current_position}")
+    #Initialise Gsheet
+    gc = pygsheets.authorize(service_account_file=gsheet_cred_path)
+  
+    print(gc.spreadsheet_titles())
+  
+    sh = gc.open('SGTD Received APIs')
+    worksheet_replit = sh.worksheet_by_title("replit_vessel_current_position")
+
+    # Extract specific keys from 'vessel_particulars' for column headers
+    vessel_particulars = data_dict['payload'][0]['vessel_particulars'][0]
+  
+    print(f"vessel_particulars: {vessel_particulars}")
+  
+    # Create column headers from the keys in 'vessel_particulars'
+    column_headers = list(vessel_particulars.keys())
+  
+    print(f"column_headers: {column_headers}")
+  
+    # Extract all the keys from the payload data
+    payload_keys = list(data_dict['payload'][0].keys())
+  
+    print(f"payload_keys: {payload_keys}")
+      # Append the payload keys (excluding 'vessel_particulars') to column_headers
+    column_headers.extend([key for key in payload_keys if key != 'vessel_particulars'])
+    
+    # Append a 'Timestamp' column
+    #Column_headers.append('Timestamp')
+    print(f"column_headers final: {column_headers}")
+
+    # Write the headers as the first row
+    worksheet_replit.insert_rows(
+    row=1,number=1,values=column_headers)
+
+    # Extract the payload data
+    payload_data = data_dict['payload'][0]
+  
+    print(f"payload_data: {payload_data}")
+  
+    # Extract all the values from the payload data
+    payload_values = [payload_data[key] for key in payload_keys if key != 'vessel_particulars']
+  
+    print(f"payload_values: {payload_data}")
+  
+    # Create a list of values corresponding to the keys
+    vessel_particulars_values = list(vessel_particulars.values())
+  
+    print(f"vessel_particulars_values: {vessel_particulars_values}")
+  
+    # Extend row_values with payload_values
+    row_values = vessel_particulars_values + payload_values
+    
+    # Append the 'Timestamp' value
+    #row_values.append(current_datetime)
+    print(f"row_values = {row_values}")
+
+    # Append the data as a new row
+    worksheet_replit.append_table(values=row_values, start='A2')
+    worksheet_replit.delete_rows(1)
+    return f"Vessel Current Location Data saved to Google Sheets.{row_values}"
