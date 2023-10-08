c6912d3b7f3f273ee51ca6437f2096273249a5a1
KavyaPai6 <kavya.pai@juegostudioz.com>
2023-10-09 00:06:48 +0530

    FEAT: Created button component

---
+++ sgtd-web/src/components/Button.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled, { RuleSet } from 'styled-components';
+import AppColors from '../styles/colors';
+
+interface ButtonProps {
+    title: String;
+    buttonStyle?:  RuleSet<object>;
+    clickHandler : React.MouseEventHandler<HTMLButtonElement>;
+}
+
+
+const Button: React.FC<ButtonProps> = ({title, buttonStyle,clickHandler}) =>{
+    return  <CustomButton onClick={clickHandler} $style={buttonStyle}>{title}</CustomButton>
+}
+
+export default Button
+
+const CustomButton = styled.button<{$style?: RuleSet<Object>}>`
+    padding: 0.5rem 1rem;
+    font-weight: 600;
+    border-radius: 0.25rem;
+    padding-bottom: 0.5rem;
+    margin: 0.25rem;
+    border:none;
+    --shadow: 0 4px 6px -1px ${AppColors.ThemePrimaryTransparencyBlack},
+    0 2px 4px -1px ${AppColors.ThemeLightTransparencyBlack};
+    box-shadow: 0 0 ${AppColors.ThemeBlack}, 0 0 ${AppColors.ThemeBlack}, 0 0 var(--black),
+    0 0 ${AppColors.ThemeBlack}, var(--shadow);
+    color: #ffffff;
+    cursor: pointer;
+    ${(props)=> props.$style}
+`
