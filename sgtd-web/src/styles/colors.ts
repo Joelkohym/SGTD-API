e8e68bd53a3a1766f55d15b3e9b2cb9728444901
KavyaPai6 <kavya.pai@juegostudioz.com>
2023-10-09 00:03:41 +0530

    FEAT: Added global colors

---
+++ sgtd-web/src/styles/colors.ts
@@ -0,0 +1,17 @@
+enum AppColors {
+    White = "#ffffff",
+    ThemeLightGrey = "#f4f4f4",
+   
+    ThemeBlack = "#000000",
+    ThemePrimaryTransparencyBlack = "#0000000f",
+    ThemeLightTransparencyBlack = "#0000001a",
+    ThemeLightGreen = "#7db43b",
+
+    ThemeBlue = "#0062ff",
+    ThemeBlueShadow = "#d5dcef",
+
+    ThemePurple = "#8610AD",
+    ThemeLightPurple = "#da61ff"
+}
+
+export default AppColors;
