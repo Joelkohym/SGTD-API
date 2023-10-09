02f8693e58490dc072e090198b425b673d203e7a
KavyaPai6 <kavya.pai@juegostudioz.com>
2023-10-09 22:55:48 +0530

    FEAT: Initial VesselQuery Page setup with layouts

---
+++ sgtd-web/src/pages/VesselQuery.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+function VesselQuery() {
+  return (
+    <Layout>
+    <div>VesselQuery</div>
+    </Layout>
+  )
+}
+
+export default VesselQuery
