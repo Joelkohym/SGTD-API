0b48787d4d61a93923bd359f440e2b806e1ff913
KavyaPai6 <kavya.pai@juegostudioz.com>
2023-10-10 23:42:42 +0530

    FEAT: Table view page UI updated

--- sgtd-web/src/pages/TableView.tsx
+++ sgtd-web/src/pages/TableView.tsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
+import { formFieldTypes } from "../lib/constants";
+import styled from "styled-components";
+import AppColors from "../styles/colors";
+import FormController from "../components/FormController";
+import { sharedButtonStyle, sharedFlexCenter } from "../styles/global";
 
-function TableView() {
+const TableView: React.FC = () => {
+  const { input, submit, text } = formFieldTypes;
+
+  const formFields = {
+    fields: [
+      {
+        name: "vessel_imo",
+        label: "Vessel IMO for Table View",
+        placeholder: "Enter Vessel IMO for Table View",
+        defaultValue: "",
+        type: input,
+        inputType: text,
+      },
+    ],
+    buttons: [
+      {
+        name: "Search",
+        type: submit,
+        onSubmitHandler: (data: any) => handleVesselQuery(data),
+        style: sharedButtonStyle,
+      },
+    ],
+  };
+
+  function handleVesselQuery(data: any) {}
   return (
     <Layout>
-        <div>TableView</div>
+      <FormContainer>
+        <Title>Table view for IMO</Title>
+        <FormController formFields={formFields} />
+      </FormContainer>
     </Layout>
-  )
-}
+  );
+};
 
-export default TableView
+export default TableView;
+
+export const FormContainer = styled.div`
+  background: ${AppColors.White};
+  width: 25rem;
+  height: 20rem;
+  ${sharedFlexCenter}
+  flex-direction: column;
+  box-shadow: 2px 2px 10px 2px ${AppColors.ThemePurple};
+`;
+
+export const Title = styled.h3`
+  padding: 1rem;
+`;
