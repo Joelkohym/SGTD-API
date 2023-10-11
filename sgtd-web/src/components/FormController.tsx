6f14cde5cb0190753738a6757ab1cfb3a5469e67
KavyaPai6 <kavya.pai@juegostudioz.com>
2023-10-11 14:22:21 +0530

    Code: Document formatted

--- sgtd-web/src/components/FormController.tsx
+++ sgtd-web/src/components/FormController.tsx
@@ -9,13 +9,13 @@ import { sharedFlexCenter, sharedFlexSpaceBetween } from "../styles/global";
 interface FormProps {
   formFields: any;
   row?: boolean;
-  isFormRow?: boolean
+  isFormRow?: boolean;
 }
 
 const FormController: React.FC<FormProps> = ({
   formFields,
   row,
-  isFormRow
+  isFormRow,
 }) => {
   const { control, handleSubmit, formState } = useForm(); //TODO: controller is used to register external component(i.e Input, dropdown) values to form.
   const { submit } = formFieldTypes;
@@ -24,51 +24,60 @@ const FormController: React.FC<FormProps> = ({
   return (
     <Form>
       <FormFieldContainer $row={isFormRow}>
-      {formFields?.fields?.map((formField : any, index: React.Key | null | undefined) => (
-        <FieldContainer $row={isFormRow}>
-        <Field $row={row}>
-              <Label>
-                {formField.label}
-              </Label>
-              {formField.defaultValue !== "undefined" && ( //Temporary approach. will be checked once api's available
-                <Controller
-                  name={formField.name}
-                  control={control}
-                  defaultValue={formField.defaultValue}
-                  render={({ field }) => (
-                    <>
-                          <Input
-                            title={formField.name}
-                            value={field.value ?? formField.defaultValue}
-                            onChangeHandler={field.onChange}
-                            type={formField.inputType}
-                            inputStyle={formField.style}
-                            required={formField.required && formField.required}
-                            defaultValue={formField.defaultValue}
-                            placeholder={formField.placeholder}
-                            readOnly={formField.readOnly}
-                            disabled={formField.disabled}
-                            enableInputStyleWithValue = {formField?.enableInputStyleWithValue}
-                          />
-                    </>
-                  )}
-                />
-              )}
-            </Field>
-          </FieldContainer>
-      ))}
+        {formFields?.fields?.map(
+          (formField: any, index: React.Key | null | undefined) => (
+            <FieldContainer $row={isFormRow}>
+              <Field $row={row}>
+                <Label>{formField.label}</Label>
+                {formField.defaultValue !== "undefined" && ( //Temporary approach. will be checked once api's available
+                  <Controller
+                    name={formField.name}
+                    control={control}
+                    defaultValue={formField.defaultValue}
+                    render={({ field }) => (
+                      <>
+                        <Input
+                          title={formField.name}
+                          value={field.value ?? formField.defaultValue}
+                          onChangeHandler={field.onChange}
+                          type={formField.inputType}
+                          inputStyle={formField.style}
+                          required={formField.required && formField.required}
+                          defaultValue={formField.defaultValue}
+                          placeholder={formField.placeholder}
+                          readOnly={formField.readOnly}
+                          disabled={formField.disabled}
+                          enableInputStyleWithValue={
+                            formField?.enableInputStyleWithValue
+                          }
+                        />
+                      </>
+                    )}
+                  />
+                )}
+              </Field>
+            </FieldContainer>
+          )
+        )}
       </FormFieldContainer>
       <div>
-        {formFields?.buttons?.map(({ name, type, onSubmitHandler,style}: any , index: React.Key | null | undefined) => (
-          <Button
-            key={index}
-            title={name}
-            clickHandler={
-              type === submit ? handleSubmit(onSubmitHandler) : onSubmitHandler
-            }
-            buttonStyle={style}
-          />
-        ))}{" "}
+        {formFields?.buttons?.map(
+          (
+            { name, type, onSubmitHandler, style }: any,
+            index: React.Key | null | undefined
+          ) => (
+            <Button
+              key={index}
+              title={name}
+              clickHandler={
+                type === submit
+                  ? handleSubmit(onSubmitHandler)
+                  : onSubmitHandler
+              }
+              buttonStyle={style}
+            />
+          )
+        )}{" "}
       </div>
     </Form>
   );
@@ -77,29 +86,29 @@ const FormController: React.FC<FormProps> = ({
 export default FormController;
 
 const Form = styled.form`
-    ${sharedFlexCenter}
-    width:100%;
-    flex-direction: column;
-`
+  ${sharedFlexCenter}
+  width:100%;
+  flex-direction: column;
+`;
 
 const FlexRow = css`
-flex-wrap:wrap;
-flex-Direction: row;
-`
+  flex-wrap: wrap;
+  flex-direction: row;
+`;
 
-const FormFieldContainer = styled.div<{$row?: boolean}>`
-${sharedFlexCenter}
-width:100%;
-flex-direction: column;
-${(props) => props.$row && FlexRow}
-`
-const FieldContainer = styled.div<{$row?: boolean}>`
+const FormFieldContainer = styled.div<{ $row?: boolean }>`
   ${sharedFlexCenter}
-  width: ${props => props.$row ? "40%" : "60%"};
-  padding: 0 2rem
-`
+  width:100%;
+  flex-direction: column;
+  ${(props) => props.$row && FlexRow}
+`;
+const FieldContainer = styled.div<{ $row?: boolean }>`
+  ${sharedFlexCenter}
+  width: ${(props) => (props.$row ? "40%" : "60%")};
+  padding: 0 2rem;
+`;
 
-const Field = styled.div<{$row?: boolean}>`
+const Field = styled.div<{ $row?: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -107,7 +116,7 @@ const Field = styled.div<{$row?: boolean}>`
   margin-bottom: 1rem;
   width: 100%;
   ${(props) => props.$row && sharedFlexSpaceBetween}
-`
+`;
 
 const Label = styled.label`
   font-weight: 600;
@@ -117,4 +126,4 @@ const Label = styled.label`
   margin-right: 1rem;
   text-transform: capitalize;
   width: max-content;
-`
+`;
