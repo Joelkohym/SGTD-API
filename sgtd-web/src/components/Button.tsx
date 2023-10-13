72bd6fc82f4f1c1073fb946f9a3e6e15c2c34938
KavyaPai6 <kavya.pai@juegostudioz.com>
2023-10-13 12:26:47 +0530

    FEAT: Button Styling updated

--- sgtd-web/src/components/Button.tsx
+++ sgtd-web/src/components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
     title: String;
     buttonStyle?:  RuleSet<object>;
     clickHandler : React.MouseEventHandler<HTMLButtonElement>;
-}
+    }
 
 
 const Button: React.FC<ButtonProps> = ({title, buttonStyle,clickHandler}) =>{
@@ -19,14 +19,13 @@ const CustomButton = styled.button<{$style?: RuleSet<Object>}>`
     padding: 0.5rem 1rem;
     font-weight: 600;
     border-radius: 0.25rem;
-    padding-bottom: 0.5rem;
     margin: 0.25rem;
     border:none;
     --shadow: 0 4px 6px -1px ${AppColors.ThemePrimaryTransparencyBlack},
     0 2px 4px -1px ${AppColors.ThemeLightTransparencyBlack};
     box-shadow: 0 0 ${AppColors.ThemeBlack}, 0 0 ${AppColors.ThemeBlack}, 0 0 var(--black),
     0 0 ${AppColors.ThemeBlack}, var(--shadow);
-    color: #ffffff;
+    color: ${AppColors.White};
     cursor: pointer;
     ${(props)=> props.$style}
 `
